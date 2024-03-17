4f5445d949e3f6ffdf30504592da56309c67e214
Mouad Kimdil <mkimdil@e3r10p8.1337.ma>
2024-03-17 16:05:17 +0000

    add message error when name is wrong

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: mkimdil <mkimdil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 20:18:21 by mkimdil           #+#    #+#             */
-/*   Updated: 2024/03/16 22:23:32 by mkimdil          ###   ########.fr       */
+/*   Updated: 2024/03/17 15:54:22 by mkimdil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ int	main(int ac, char **av)
 	t_vars		vars;
 
 	if (ac != 2)
-		return (ft_putendl_fd(ARGS_MSG, 1), 0);
+		return (ft_putendl_fd(ARGS_MSG, 2), 1);
 	if (WIDTH <= 0 || WIDTH > 2560 || HEIGHT <= 0 || HEIGHT > 1440)
 		my_exit(WRONG_RESOLUTION);
-	if (ft_strncmp(av[1], "julia", 6) == 0)
-		julia_call(&julia);
 	else if (ft_strncmp(av[1], "mandelbrot", 11) == 0)
 		mandelbrot_call(&vars);
+	if (ft_strncmp(av[1], "julia", 6) == 0)
+		julia_call(&julia);
 	else if (ft_strncmp(av[1], "burning_ship", 13) == 0)
 		my_fractal_call(&vars);
+	else
+		return (ft_putendl_fd(ARGS_MSG, 2), 1);
 	return (0);
 }
