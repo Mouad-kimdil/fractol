6053ff7c5711602a074a5ed3443e6e712bc882c0
Mohammed Amine Es-Salhi <mes-salh@e3r10p4.1337.ma>
2024-08-01 05:42:54 +0100

    it was bad this is good now

--- mandelbrot.c
+++ mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: mes-salh <mes-salh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 23:25:06 by mkimdil           #+#    #+#             */
-/*   Updated: 2024/08/01 05:40:26 by mes-salh         ###   ########.fr       */
+/*   Updated: 2024/08/01 05:42:24 by mes-salh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	initialize_mandelbrot(t_vars *vars)
 	vars->center_imaginary = 0.0;
 	vars->scale = 1.0;
 	vars->gen_color = 5;
-	vars->max_iter = 100;
+	vars->max_iter = 300;
 	vars->move = 2.0;
 	vars->move1 = 2.0;
 	vars->move_step = 0.09;
